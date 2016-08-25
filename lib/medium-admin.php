9b7513b06dc831c23310e50da619ef384330c918
davpsh <davps@bk.ru>
2016-08-26 03:57:50 +0700

    wp_get_the_category() chnged to wp_get_post_categories() Added comment for wp_get_post_categories()

--- lib/medium-admin.php
+++ lib/medium-admin.php
@@ -765,12 +765,9 @@ class Medium_Admin {
     $tags = array_values(array_unique(array_merge($slugs, $tags)));
 
     // pull in categories as tags
-    $categories = wp_get_the_category($post->ID);
-    $cats = array();
-    foreach($categories as $category){
-        $cats[] = $category->name;
-    }
-    $tags = array_values(array_unique(array_merge($cats, $tags)));
+    $args = array('fields' => 'names'); // @see wp_get_post_categories() $args ref to wp_get_object_terms()
+    $categories_name = wp_get_post_categories($post->ID, $args);
+    $tags = array_values(array_unique(array_merge($categories_name, $tags)));
 
     if (class_exists('CoAuthors_Guest_Authors')) {
       // Handle guest-authors if the CoAuthors Plus plugin is installed.
